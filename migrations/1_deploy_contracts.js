bb3e143a139714353ecf6791d4a0edd1e776ed13
José Molina <jose.molina@criptan.com>
2022-06-13 17:37:49 +0200

    Use environment variables to handle deployment

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -2,10 +2,12 @@ const Wallet = artifacts.require('Wallet')
 const WalletFactory = artifacts.require('WalletFactory')
 
 module.exports = async (deployer, network, accounts) => {
-  const master = network === 'mainnet' ? '0x10327Ea75dF3935f5eAD94F5C02Cc2378DB936c3' : accounts[0]
+  const master = network === 'mainnet' ? process.env.MASTER : accounts[0]
   await deployer.deploy(Wallet)
   const template = await Wallet.deployed()
   await deployer.deploy(WalletFactory, master, template.address)
-  const walletFactoryInstance = await WalletFactory.deployed()
-  await walletFactoryInstance.transferOwnership(master)
+  if (process.env.OWNER) {
+    const walletFactoryInstance = await WalletFactory.deployed()
+    await walletFactoryInstance.transferOwnership(process.env.OWNER)
+  }
 }
